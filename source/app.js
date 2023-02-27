ef22e5b443e6355111fd81bb04d17ad3bf489634
nihalbaruah <85226892+nihalbaruah@users.noreply.github.com>
2023-02-27 12:34:49 +0530

    Update app.js

--- source/app.js
+++ source/app.js
@@ -16,7 +16,10 @@ const partialsPath = path.join(__dirname,'../templates/partials');
 app.set('view engine','hbs');
 app.set('views',viewsPath);
 hbs.registerPartials(partialsPath);
-app.use(express.static(publicStaticDirPath));
+//app.use(express.static(publicStaticDirPath));
+
+app.use('/.netlify/source/app', router);
+module.exports.handler = serverless(app);
 
 app.get('',(req,res) => {
     res.render('index',{
